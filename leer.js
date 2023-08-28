3aebdbdc8e95cb1ff6ac069da0cdd048e134087e
pher2017 <pghernandez@gmail.com>
2023-08-28 15:20:36 -0400

    Update

--- leer.js
+++ leer.js
@@ -1,33 +1,34 @@
 const vue = new Vue({
- el:'#app',
- data:{
-  listaDatos : []
- },
- created(){
-  this.getLista()
- },
- methods:{
-  recargar(){
-   console.log("recargando");
-   this.getLista()
+  el: '#app',
+  data: {
+    listaDatos: []
   },
-getLista(){
- // id de la hoja de calculo
- idSheets = '1CNZhgEz1aEzoRA32rtS87iArFlomyl6noKIQPH0HTec';
- //// nuestra      APIKey
- apiKey = 'AIzaSyBqN-fDN62EuWBTww5EXdx1nNA9EsqHgSo'; 
- // rango de la hoja de calculo que queremos leer
- values = 'A2:N32';
-// fetch es un método nativo para hacer peticiones http
-// en el navegador 
- fetch("https://content-sheets.googleapis.com/v4/spreadsheets/" +   idSheets + "/values/A2:AZ100?access_token="+ apiKey +"&key="+  apiKey)
-.then((lista)=>{
-  return lista.json()
-}).then((valores)=>{
-  this.listaDatos = valores.values
-}).catch(err=>{
-  console.log(err);
-})
-} // fin funcion getLista()
-} // fin methods
+  created() {
+    this.getLista()
+  },
+  methods: {
+    recargar() {
+      console.log("recargando");
+      this.getLista()
+    },
+    getLista() {
+      // id de la hoja de calculo
+      idSheets = '1CNZhgEz1aEzoRA32rtS87iArFlomyl6noKIQPH0HTec';
+      //// nuestra      APIKey
+      apiKey = 'AIzaSyBqN-fDN62EuWBTww5EXdx1nNA9EsqHgSo';
+      // rango de la hoja de calculo que queremos leer
+      values = 'A2:N32';
+      // fetch es un método nativo para hacer peticiones http
+      // en el navegador 
+      fetch("https://content-sheets.googleapis.com/v4/spreadsheets/" + idSheets + "/values/A2:AZ100?access_token=" + apiKey + "&key=" + apiKey)
+        .then((lista) => {
+          return lista.json()
+        }).then((valores) => {
+          this.listaDatos = valores.values
+          console.log(this.listaDatos,'datos')
+        }).catch(err => {
+          console.log(err);
+        })
+    } // fin funcion getLista()
+  } // fin methods
 }) // fin instancia
